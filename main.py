52ff4a39978f9f9322e0bbd7b759586363af1f69
harimang <thunnes@hotmail.com>
2025-07-06 17:59:14 -0700

    added file reading

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(file):
+
+    file_contents = ""
+
+    with open(file) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+
+main()
